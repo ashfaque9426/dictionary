68109d9569b17a8b67373e513c39f7711e97e3af
ashfaque9426 <121668444+ashfaque9426@users.noreply.github.com>
2023-02-26 23:29:54 +0600

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,3 +1,4 @@
+// caching dom element data by selecting them
 const inputField = document.getElementById("searchInput");
 const searchBtn = document.getElementById("search");
 const dictionaryContainer = document.getElementById("dicContainer");
@@ -8,19 +9,32 @@ const fetchDicData = async () => {
         dictionaryContainer.innerHTML = "";
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputField.value.toLowerCase()}`;
         const res = await fetch(url);
-        if(res.status === 404) {
-            dictionaryContainer.innerText = "No Valid Word Found According To Your Search";
-            return;
+        // if you get the data correctly checking by res.ok
+        if(res.ok) {
+            const data = await res.json();
+            // console.log(data);
+            displayData(data);
+            
+        } else {
+            // dom output with different seach situations
+            if(inputField.value === "") {
+                dictionaryContainer.innerText = "You havent inserted word for searching";
+                throw new Error("not found. error code: " + res.status);
+            } else {
+                dictionaryContainer.innerText = "No Valid Word Found According To Your Search";
+                throw new Error("not found. error code: " + res.status);
+            }
+            
         }
-        const data = await res.json();
-        // console.log(data);
-        displayData(data);
+        
     }
     catch(error) {
+        // consoling the error
         console.log(error);
     }
 }
 
+// displaying data to the dom
 const displayData = (data) => {
     const dicData = data[0];
     // console.log(dicData);
@@ -31,7 +45,13 @@ const displayData = (data) => {
 
     const whichPartsOfSpeech = [];
     const nounDescArr = [];
+    const pronounDescArr = [];
     const verbDescArr = [];
+    const adverbDescArr = [];
+    const adjectiveDescArr = [];
+    const prepositionDescArr = [];
+    const conjunctionDescArr = [];
+    const interjectionDescArr = [];
 
     meanings.forEach(obj => {
         if (obj["partOfSpeech"] === "noun" && obj["definitions"] !== null) {
@@ -42,6 +62,31 @@ const displayData = (data) => {
             whichPartsOfSpeech.push(obj["partOfSpeech"]);
             obj["definitions"].forEach(obj => verbDescArr.push(obj["definition"]));
         }
+        if (obj["partOfSpeech"] === "adjective" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => adjectiveDescArr.push(obj["definition"]));
+        }
+        if (obj["partOfSpeech"] === "preposition" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => prepositionDescArr.push(obj["definition"]));
+        }
+        if (obj["partOfSpeech"] === "conjunction" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => conjunctionDescArr.push(obj["definition"]));
+        }
+        if (obj["partOfSpeech"] === "interjection" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => interjectionDescArr.push(obj["definition"]));
+        }
+        if (obj["partOfSpeech"] === "pronoun" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => pronounDescArr.push(obj["definition"]));
+        }
+        if (obj["partOfSpeech"] === "adverb" && obj["definitions"] !== null) {
+            whichPartsOfSpeech.push(obj["partOfSpeech"]);
+            obj["definitions"].forEach(obj => adverbDescArr.push(obj["definition"]));
+        }
+        
     });
 
 
@@ -51,52 +96,114 @@ const displayData = (data) => {
         <header>
             <h1>${word}</h1>
             <p>${phonetics[0]["text"] === undefined ? phonetics[1]["text"] : phonetics[0]["text"]}</p>
-            <p>${meanings[0]["partOfSpeech"]}</p>
         </header>
-        <article>
-            <h2>Meaning</h2>
-            <ul id="nounDescContainer">
-            </ul>
-            <h2></h2>
-        </article>
-        <article>
-            <h2 id="synonym">Synonym: ${meanings[0]["synonyms"][0] === undefined ? "not found" : meanings[0]["synonyms"][0]}</h2>
-            <p>${meanings[1] === undefined? "" : meanings[1]["partOfSpeech"]}</p>
-            <h2>${whichPartsOfSpeech.includes("verb")? "Meaning" : ""}</h2>
-            <ul id="verbDescContainer">
-                
-            </ul>
-        </article>
+        <h2 id="synonym">Synonym: ${meanings[0]["synonyms"][0] === undefined ? "not found" : meanings[0]["synonyms"][0]}</h2>
+        <section id="info"></section>
         <footer><a target="_blank" href="${sourceUrls}">${sourceUrls}</a></footer>
         <button class="mt-3" id="btn" onclick="playAudio('${phonetics[0]['audio']}')">Play Audio</button>
     `;
 
-    dictionaryContainer.appendChild(article); //verbDescContainer
+    dictionaryContainer.appendChild(article);
 
     if(nounDescArr.length !== 0) {
+        displayDictInfo("noun");
         nounDescArr.forEach(str => {
             const li = document.createElement("li");
             li.innerText = str;
-
-            document.getElementById("nounDescContainer").appendChild(li);
+            document.getElementById("nounContainer").appendChild(li);
         });
     }
 
     if(verbDescArr.length !== 0) {
+        displayDictInfo("verb");
         verbDescArr.forEach(str => {
             const li = document.createElement("li");
             li.innerText = str;
 
-            document.getElementById("verbDescContainer").appendChild(li);
+            document.getElementById("verbContainer").appendChild(li);
+        });
+    }
+
+    if (adjectiveDescArr.length !== 0) {
+        displayDictInfo("adjective");
+        adjectiveDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("adjectiveContainer").appendChild(li);
+        });
+    }
+
+    if (prepositionDescArr.length !== 0) {
+        displayDictInfo("preposition");
+        prepositionDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("prepositionContainer").appendChild(li);
+        });
+    }
+
+    if (conjunctionDescArr.length !== 0) {
+        displayDictInfo("conjunction");
+        conjunctionDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("conjunctionContainer").appendChild(li);
+        });
+    }
+
+    if (interjectionDescArr.length !== 0) {
+        displayDictInfo("interjection");
+        interjectionDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("interjectionContainer").appendChild(li);
+        });
+    }
+
+    if (pronounDescArr.length !== 0) {
+        displayDictInfo("pronoun");
+        pronounDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("pronounContainer").appendChild(li);
+        });
+    }
+
+    if (adverbDescArr.length !== 0) {
+        displayDictInfo("pronoun");
+        adverbDescArr.forEach(str => {
+            const li = document.createElement("li");
+            li.innerText = str;
+
+            document.getElementById("adverbContainer").appendChild(li);
         });
     }
 
 
 }
 
+// dictionary info function from preventing the same code over and over again
+const displayDictInfo = (id) => {
+    const article = document.createElement("article");
+    article.innerHTML = `
+            <p class="mt-3 font-bold">${id}</p>
+            <h2>Meaning</h2>
+            <ul id="${id}Container" class="mb-3">
+            </ul>
+        `;
+    document.getElementById("info").appendChild(article);
+}
+
+// for audio
 const playAudio = (url) => {
     const beat = new Audio(url);
     beat.play();
 }
 
+// event handler to the seach word button
 searchBtn.addEventListener("click", fetchDicData);
